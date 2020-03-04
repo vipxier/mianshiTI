709fa01111cbaa5fcaa0e0ddfc15c75dea3d9327
macbook <macbook@macbooks-mbp>
2020-03-04 07:11:41 -0600

    two more questions' answer

--- YAMA/src/shuatiwang_yama_array_string/ConvertStringToInteger.java
+++ YAMA/src/shuatiwang_yama_array_string/ConvertStringToInteger.java
@@ -1,5 +1,75 @@
 package shuatiwang_yama_array_string;
 
 public class ConvertStringToInteger {
+	public int myAtoi(String str) {
+		if(str == null || str.length() == 0)
+        {
+            return 0;
+        }
+        
+        int start = 0;
+        int end = str.length() - 1;
+        
+        while(start < str.length() && (str.charAt(start) == ' '))
+        {
+            start ++;
+        }
+        
+        while(end >= 0 && !Character.isDigit(str.charAt(end)))
+        {
+            end --;
+        }
+        
+        if(end < start)
+        {
+            return 0;
+        }
+        
+        int sign = 1;
+        if(str.charAt(start) == '-')
+        {
+            sign = -1;
+            start += 1;
+        }
+        else if(str.charAt(start) == '+')
+        {
+            start ++;
+        }
+        
+        double value = 0;
+        for(int i = start; i <= end ; i ++)
+        {
+            if(str.charAt(i) >='0' && str.charAt(i) <= '9'){
+                value = value * 10 + str.charAt(i) - '0';
+            }
+            else
+            {
+               break;
+            }
+        }
+    
+        value *= sign;
+        if(value > Integer.MAX_VALUE)
+        {
+            value = Integer.MAX_VALUE;
+        }
+        else if(value <  Integer.MIN_VALUE)
+        {
+            value = Integer.MIN_VALUE;
+        }
+        
+        return (int) (value);
+    }
+	
+	public static void main(String[] args)
+	{
+		String str = "42";
+		ConvertStringToInteger solution = new ConvertStringToInteger();
+		System.out.println(solution.myAtoi(str));
+		System.out.println(solution.myAtoi("    -42"));
+		System.out.println(solution.myAtoi("    -42 abcd  aa"));
 
+		System.out.println(solution.myAtoi("    -42.1315 ab12cd aa"));
+		
+	}
 }
--- YAMA/src/shuatiwang_yama_array_string/LonggestStringWithoutRepeatedCharacter.java
+++ YAMA/src/shuatiwang_yama_array_string/LonggestStringWithoutRepeatedCharacter.java
@@ -1,7 +1,35 @@
 package shuatiwang_yama_array_string;
 
 public class LonggestStringWithoutRepeatedCharacter {
-public int lengthOfLongestSubstring(String s) {
+	public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0)
+        {
+            return 0;
+        }
         
+        if(s.length() == 1)
+        {
+            return 1;
+        }
+        
+        int startLocation = 0;
+        int maxLength = 1;
+        
+        for(int i = 1; i < s.length() ; i++)
+        {
+            String currentString = s.substring(startLocation, i);
+            if(currentString.indexOf(s.charAt(i)) != -1)
+            {
+                maxLength = Math.max(maxLength, i - startLocation);
+                startLocation = startLocation + currentString.indexOf(s.charAt(i)) + 1;
+            }
+        }
+        
+        if(startLocation < s.length())
+        {
+            maxLength = Math.max(maxLength, s.length() - startLocation);
+        }
+        
+        return maxLength;
     }
 }
